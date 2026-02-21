2bba3131dc84c290ac7c5e34633e56021d720100
Hammad Ahmad <hammadahmad.9022@gmail.com>
2026-02-21 15:31:54 +0500

    build for git hub pages

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/Sajdah/", 
   plugins: [
     tailwindcss(),
     react({
-      babel: {
-        base: "/Sajdah/",
-        plugins: [['babel-plugin-react-compiler']],
-      },
+      plugins: [['babel-plugin-react-compiler']],
     }),
   ],
 })
